603704a7352d0a3f92d4afa5b2121cf3d2a5c8ea
bdickason <dickason@gmail.com>
2014-02-08 18:08:05 -0500

    Updating heroku settings

--- server/db.js
+++ server/db.js
@@ -22,11 +22,11 @@ module.exports.setup = function(cfg, callback) {
 
   // Initial setup and connection for database
   r.connect({ host: dbConfig.host, port: dbConfig.port, db: dbConfig.db, authKey: dbConfig.authKey }, function(err, conn) {
+    console.log(dbConfig);
     if(err) {
       throw err;
     }
     else {
-      console.log(dbConfig.host);
       // Check if tables already exist. If not, create them
       r.dbCreate(dbConfig.db).run(conn, function(err, result) {
         if(err) {

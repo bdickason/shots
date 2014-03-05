4ceebd813ac5e62cf7e4593040efe78190e35306
bdickason <dickason@gmail.com>
2014-03-05 18:24:31 -0500

    Added test for properly rendering a list of comments

--- test/app/commentView.spec.js
+++ test/app/commentView.spec.js
@@ -11,12 +11,13 @@ describe('commentView', function() {
 
   beforeEach(function(done) {
     clientenv.setup(function() {
+      
       // Pre-compile Handlebars template
       var templateFilename = path.resolve(__dirname, componentsDir + 'comments/commentTemplate.hbs');
       var commentTemplate = require('handlebars').compile(templateFilename);
 
+      // Setup objects
       CommentView = require(componentsDir + 'comments/commentView.js');
-
       CommentModel = Backbone.Model;  // Dummy model to pass into view
 
       done();
@@ -31,4 +32,24 @@ describe('commentView', function() {
       should.exist(commentView);
     });
   });
+
+  describe('render', function() {
+    it('creates an <li> with id and the proper class', function() {
+      // Input
+      var id = 'test';
+
+      commentModel = new CommentModel({id: id});
+      commentView = new CommentView({model: commentModel});
+
+      commentModel.trigger('change'); // Grabbing data from firebase would trigger change event, forcing view to render()
+
+      commentView.$el.children.length.should.equal(2);
+
+      commentList = commentView.$el.find('li');
+      should.exist(commentList);
+      should.exist(commentList.attr('id'));
+      commentList.attr('id').should.equal(id);
+    });
+  });
+
 });

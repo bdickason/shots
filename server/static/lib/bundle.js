dcc5b2747087b88e9d18ebd4fbc872234a7156a3
bdickason <dickason@gmail.com>
2014-03-08 12:41:08 -0500

    Added code and tests for an 'owner' flag on Shot Model #8

--- server/static/lib/bundle.js
+++ server/static/lib/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* Main app js file */
 
-userModel = require('./components/users/userModel.js');
+var userModel = require('./components/users/userModel.js');
 app = {};
 app.views = [];
 
@@ -813,7 +813,14 @@ module.exports = Backbone.Firebase.Model.extend({
       text: ''
     },
     toJSON: function() {
-        return(utils.formatTime(this));         // Generate human-readable timestamp
+      var output = utils.formatTime(this);  // Generate human-readable timestamp
+      
+      if(this.get('user') === app.user.get('username')) {
+        // User owns this comment
+        output.owner = true;
+      }
+      
+      return(output);         // Generate human-readable timestamp
     }
 });
 

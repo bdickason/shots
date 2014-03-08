d98c6b371a48e32797c5402a603bc0b2ca0fe67a
bdickason <dickason@gmail.com>
2014-03-08 14:08:09 -0500

    Added ability to determine owner of project models

--- app/components/projects/projectModel.js
+++ app/components/projects/projectModel.js
@@ -1,6 +1,25 @@
 /* Project Model - data layer for a single Project for use in Firebase Collections */
 
+var utils = require('../../utils.js');
+
 module.exports = Backbone.Model.extend({
   initialize: function() {
+  },
+  isOwner: function(user) {
+    if(this.get('user') == user) {
+      return(true);
+    } else {
+      return(false);
+    }
+  },
+  toJSON: function() {
+    var output = utils.formatTime(this);  // Generate human-readable timestamp
+    
+    if(this.get('user') === app.user.get('username')) {
+      // User owns this comment
+      output.owner = true;
+    }
+    
+    return(output);         // Generate human-readable timestamp
   }
 });
--- app/components/projects/projectModelFirebase.js
+++ app/components/projects/projectModelFirebase.js
@@ -1,10 +1,29 @@
 /* Project Model - For standalone use (not in a collection) */
 
+var utils = require('../../utils.js');
+
 module.exports = Backbone.Firebase.Model.extend({
   firebase: function() {
     return(new Firebase(this.fbUrl));
   },
   initialize: function() {
     this.fbUrl = app.fbUrl + '/projects/' + this.get('id');
+  },
+  isOwner: function(user) {
+    if(this.get('user') == user) {
+      return(true);
+    } else {
+      return(false);
+    }
+  },
+  toJSON: function() {
+    var output = utils.formatTime(this);  // Generate human-readable timestamp
+    
+    if(this.get('user') === app.user.get('username')) {
+      // User owns this comment
+      output.owner = true;
+    }
+    
+    return(output);         // Generate human-readable timestamp
   }
 });

d4fb0e3be4cdfc8be88f22ae2930bff5b685b258
bdickason <dickason@gmail.com>
2014-03-14 19:17:24 -0400

    Refactored navView to use Marionette ItemView

--- server/static/lib/bundle.js
+++ server/static/lib/bundle.js
@@ -443,14 +443,13 @@ function program3(depth0,data) {
 
 var navTemplate = require('./navTemplate.hbs');
 
-module.exports = Backbone.View.extend({
+module.exports = Backbone.Marionette.ItemView.extend({
   tagName: 'div',
 
   template: navTemplate,
 
   initialize: function() {
     this.listenTo(this.model, 'change', this.render); // Without this, the collection doesn't render after it completes loading
-    this.render();
   },
 
   events: {
@@ -459,11 +458,6 @@ module.exports = Backbone.View.extend({
     'click #logout': 'logout',
     'click #contribute': 'contribute',
     'click #help': 'help'
-  },
-
-  render: function() {
-    this.$el.html(this.template(this.model.toJSON())); // Nav has no collection associated with it, so just render the tepmlate
-    return this;
   },
 
   home: function(e) {

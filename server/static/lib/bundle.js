2eaa0c8d00f93ea392a3431719c0ffc4ae3a4fee
bdickason <dickason@gmail.com>
2014-03-19 19:22:30 -0400

    Renamed usermodel to loginmodel

--- server/static/lib/bundle.js
+++ server/static/lib/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* Main app js file */
 
-var userModel = require('./components/users/userModel.js');
+var userModel = require('./components/users/loginModel.js');
 app = new Backbone.Marionette.Application();
 app.views = [];
 
@@ -30,7 +30,7 @@ window.onload = function(){
 };
 
 
-},{"./components/users/userModel.js":36,"./routes.js":37,"./utils.js":38}],2:[function(require,module,exports){
+},{"./components/users/loginModel.js":36,"./routes.js":37,"./utils.js":38}],2:[function(require,module,exports){
 /* Comment Model - data layer for a single Comment */
 
 var utils = require('../../utils.js');

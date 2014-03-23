bc564d5337a109050f4fd5d485ed9112b596c21a
bdickason <dickason@gmail.com>
2014-03-23 19:33:46 -0400

    Updated gruntfile for heroku deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,7 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('heroku:production', ['browserify', 'compass']);
   grunt.registerTask('default', ['browserify', 'compass']); // By default run tests
   grunt.registerTask('tests', 'mochaTest');   // Only run tests 
 };

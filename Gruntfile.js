4a6071d5fa3ec698a8ff023d5fe20fa7af5fcae2
bdickason <dickason@gmail.com>
2014-03-26 21:53:24 -0400

    Testing other heroku config

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,9 +51,9 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('heroku:production', ['browserify', 'compass']);
+  // grunt.registerTask('heroku:production', ['browserify', 'compass']);
   grunt.registerTask('default', ['browserify', 'compass']); // By default run tests
   grunt.registerTask('tests', 'mochaTest');   // Only run tests 
   grunt.registerTask('build', ['browserify', 'compass', 'mochaTest']);
-  grunt.registerTask('heroku:production', ['browserify, compass']);
+  grunt.registerTask('heroku:production', 'browserify, compass');
 };

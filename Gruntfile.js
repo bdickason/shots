26f83fb3a5b633923525c5ddbec82b980c44f605
bdickason <dickason@gmail.com>
2014-03-30 23:24:37 -0700

    Renamed grunt test command for easier atom integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
 
   // grunt.registerTask('heroku:production', ['browserify', 'compass']);
   grunt.registerTask('default', ['browserify', 'compass']); // By default run tests
-  grunt.registerTask('tests', 'mochaTest');   // Only run tests 
+  grunt.registerTask('test', 'mochaTest');   // Only run tests
   grunt.registerTask('build', ['browserify', 'compass', 'mochaTest']);
   grunt.registerTask('heroku:production', ['browserify', 'compass']);
 };
